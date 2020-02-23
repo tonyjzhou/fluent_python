045a584cbc91e6c0c3f849ccf7936e132159ccc2
Tony ZHOU <tony.zhoujingtao@gmail.com>
2020-02-23 11:13:16 -0800

    Add french_deck.py

---
+++ fluent_python/data_model/french_deck.py
@@ -0,0 +1,17 @@
+import collections
+
+Card = collections.namedtuple("Card", ["rank", "suit"])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = "spades hearts clubs diamonds".split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for rank in self.ranks for suit in self.suits]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, item):
+        return self._cards[item]
