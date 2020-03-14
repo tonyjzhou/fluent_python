e3cb31bbc80fcb9b9ac2d6f3cfb8db4d0f862de2
Tony ZHOU <tony.zhoujingtao@gmail.com>
2020-03-15 06:36:35 +0800

    Add tail.py

---
+++ fluent_python/collections/tail.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+
+def tail(file_name):
+    with open(file_name) as f:
+        return deque(f, 10)
+
+
+def main():
+    process("StrKeyDict.py")
+
+
+def process(file_name):
+    display(file_name, tail(file_name))
+
+
+def display(file_name, lines):
+    print(f"Last {len(lines)} lines of {file_name} are ...")
+    print("".join(lines))
+
+
+if __name__ == "__main__":
+    main()
