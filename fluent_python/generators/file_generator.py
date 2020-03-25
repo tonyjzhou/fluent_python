a888c7510df9897d565d8526d3eee44a229df754
Tony ZHOU <tony.zhoujingtao@gmail.com>
2020-03-25 14:45:48 +0800

    Add file_generator.py

---
+++ fluent_python/generators/file_generator.py
@@ -0,0 +1,31 @@
+"""
+Iterate every files under a directory and print out the lines.
+"""
+import os
+from pathlib import Path
+
+
+def print_content(path):
+    with open(path) as file:
+        print(path)
+        print("-" * len(str(path)))
+
+        for line in file:
+            print(line[:len(line) - 1])
+        print()
+
+
+def visit(directory=".", fun=print):
+    for filename in os.listdir(directory):
+        path = Path(directory, filename)
+
+        if path.exists():
+            if path.is_file():
+                if path.suffix == ".py":
+                    fun(path)
+            else:
+                visit(directory=path, fun=fun)
+
+
+# visit(fun=print_content)
+visit(directory="/Users/tonyzhou/Workspace/fluent_python", fun=print_content)
