483531098845466db3b039374c6ecf0cc2383588
Tony ZHOU <tony.zhoujingtao@gmail.com>
2021-11-07 08:38:50 -0600

    Add promotions strategies with decorator

---
+++ fluent_python/decorator/promotions.py
@@ -0,0 +1,28 @@
+promotions = []
+
+
+def promotion(prom_fun):
+    promotions.append(prom_fun)
+    return prom_fun
+
+
+@promotion
+def fidelity_promotion(order):
+    return order.total() * 0.05 if order.customer.fidelity >= 1000 else 0
+
+
+@promotion
+def bulk_item_promotion(order):
+    discount = 0
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * 0.1
+    return discount
+
+
+@promotion
+def large_order_promotion(order):
+    distinct_items = {item.quantity for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * 0.07
+    return 0
