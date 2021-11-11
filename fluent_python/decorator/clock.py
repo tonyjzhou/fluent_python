d421e7804c36ed2c60948345770d0c41a714911e
Tony ZHOU <tony.zhoujingtao@gmail.com>
2021-11-11 08:17:43 -0600

    Use functools.wraps to allow kwargs

--- fluent_python/decorator/clock.py
+++ fluent_python/decorator/clock.py
@@ -1,13 +1,21 @@
+import functools
 import time
 
 
 def clock(fun):
-    def clocked(*args):
+    @functools.wraps(fun)
+    def clocked(*args, **kwargs):
         t0 = time.perf_counter()
-        result = fun(*args)
+        result = fun(*args, **kwargs)
         duration = time.perf_counter() - t0
         fun_name = fun.__name__
-        arg_str = ', '.join(repr(a) for a in args)
+        arg_list = []
+        if args:
+            arg_list.append(", ".join(repr(a) for a in args))
+        if kwargs:
+            pairs = ["%s=%r" % (k, w) for k, w in sorted(kwargs.items())]
+            arg_list.append(", ".join(pairs))
+        arg_str = ", ".join(arg_list)
         print("[%0.8fs] %s(%s) -> %s" % (duration, fun_name, arg_str, result))
         return result
 
