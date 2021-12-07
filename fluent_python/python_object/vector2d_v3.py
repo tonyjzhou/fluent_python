208516891ce938cd8a136123bf977e8efe703005
Tony ZHOU <tony.zhoujingtao@gmail.com>
2021-12-07 08:06:57 -0600

    Make Vector immutable and hashable

---
+++ fluent_python/python_object/vector2d_v3.py
@@ -0,0 +1,49 @@
+import math
+from array import array
+
+
+class Vector2d:
+    type_code = 'd'
+
+    def __init__(self, x, y):
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return "{}({!r}, {!r})".format(class_name, *self)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return bytes([ord(self.type_code)]) + bytes(array(self.type_code, self))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
+    @classmethod
+    def from_bytes(cls, octets):
+        type_code = chr(octets[0])
+        content = memoryview(octets[1:]).cast(type_code)
+        return cls(*content)
