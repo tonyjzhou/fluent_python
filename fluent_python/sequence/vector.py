4155e39a0f9ee39c705876e7615204159c972d54
Tony ZHOU <tony.zhoujingtao@gmail.com>
2021-12-28 08:21:53 -0600

    Simplify __eq__

--- fluent_python/sequence/vector.py
+++ fluent_python/sequence/vector.py
@@ -29,12 +29,15 @@ class Vector:
 
     def __eq__(self, other):
         # return tuple(self) == tuple(other)
-        if len(self) != len(other):
-            return False
-        for a, b in zip(self, other):
-            if a != b:
-                return False
-        return True
+
+        # if len(self) != len(other):
+        #     return False
+        # for a, b in zip(self, other):
+        #     if a != b:
+        #         return False
+        # return True
+
+        return len(self) == len(other) and all(a == b for a, b in zip(self, other))
 
     def __hash__(self):
         # hashes = (hash(c) for c in self.__components)
